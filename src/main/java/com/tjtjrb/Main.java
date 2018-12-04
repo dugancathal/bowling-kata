6be9d4d30f769670f3b828529d00dec1468f8b74
TJ Taylor <dugancathal@gmail.com>
2018-12-04 14:07:00 -0700

    Initial commit

---
+++ src/main/java/com/tjtjrb/Main.java
@@ -0,0 +1,4 @@
+package com.tjtjrb;
+
+class Main {
+} 
---
+++ src/test/java/com/tjtjrb/MainTest.java
@@ -0,0 +1,15 @@
+package com.tjtjrb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(JUnit4.class)
+class MainTest {
+    @Test
+    public void foo() {
+        assertTrue(true);
+    }
+}
